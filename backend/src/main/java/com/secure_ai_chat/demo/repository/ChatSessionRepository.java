8882470aae4de52864a92b666916f089c7a5d6b6
Surendhar.K <surendhar.k@grootan.com>
2026-04-19 13:09:49 +0530

    Revert "full project"
    This reverts commit 8f50680fbb0a734143ddce290cf30819ddb35ead.

--- backend/src/main/java/com/secure_ai_chat/demo/repository/ChatSessionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.secure_ai_chat.demo.repository;
-
-import com.secure_ai_chat.demo.model.ChatSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatSessionRepository extends JpaRepository<ChatSession, Long> {
-}
